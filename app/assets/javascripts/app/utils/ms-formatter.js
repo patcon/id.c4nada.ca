84ec69bcc5e9fea7642567d134aceab584874a84
Adam Biagianti <adam.biagianti@gsa.gov>
2016-12-02 10:00:05 -0500

    Use numbers only infavor of words and numbers
    **Why**:
    Translating the mix of number and words used to convery time is a pain

--- app/assets/javascripts/app/utils/ms-formatter.js
+++ app/assets/javascripts/app/utils/ms-formatter.js
@@ -1,26 +1,17 @@
-const I18n = window.LoginGov.I18n;
-
-function pluralize(word, count) {
-  return `${word}${count !== 1 ? 's' : ''}`;
-}
-
 function formatMinutes(minutes) {
   if (!minutes) return 0;
 
-  return `${minutes} ${pluralize('minute', minutes)}`;
+  return minutes;
 }
 
 function formatSeconds(seconds) {
-  return `${seconds} ${pluralize('second', seconds)}`;
+  return seconds < 10 ? `0${seconds}` : seconds;
 }
 
 export default (milliseconds) => {
   const seconds = milliseconds / 1000;
-  const minutes = parseInt(seconds / 60, 10);
-  const remainingSeconds = parseInt(seconds % 60, 10);
+  const minutes = formatMinutes(parseInt(seconds / 60, 10));
+  const remainingSeconds = formatSeconds(parseInt(seconds % 60, 10));
 
-  const displayMinutes = I18n.t('shared.time.minutes', { count: minutes });
-  const displaySeconds = I18n.t('shared.time.seconds', { count: remainingSeconds });
-
-  return `${displayMinutes} and ${displaySeconds}`;
+  return `${minutes}:${remainingSeconds}`;
 };
