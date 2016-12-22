281292b2fd7234223af3a9efc7f5de285f4f3aa5
Moncef Belyamani <moncef.belyamani@gsa.gov>
2016-12-22 15:14:22 -0500

    Improve logic for allowing access to Verify pages
    **Why**:
    - Users who have already completed identity verification should not
    be able to directly access the `/verify/cancel`, `/verify/fail`, and
    `/verify/retry` pages because those are only meant for people who have
    not verified yet.
    - Users who have not already been verified should not be able to access
    the `/verify/activated` page because that is only meant for those who
    already have an active profile.
    - Only users who have exceeded the max allowable proofing attempts
    should be allowed to access the `/verify/fail` page.
    - The "Back" link on the `/verify/activated` page that points to the
    `/verify` endpoint doesn't make sense because it will send right back
    to the `/verify/activated` page. I removed the link because the only
    place where it makes sense to link back to is the profile page, which
    is already accessible via the "My account" link or the login.gov logo.

--- app/controllers/verify_controller.rb
+++ app/controllers/verify_controller.rb
@@ -2,9 +2,10 @@ class VerifyController < ApplicationController
   include IdvSession
 
   before_action :confirm_two_factor_authenticated
+  before_action :require_no_active_profile, only: [:cancel, :fail, :retry]
 
   def index
-    if current_user.active_profile.present?
+    if active_profile?
       redirect_to verify_activated_path
     else
       analytics.track_event(Analytics::IDV_INTRO_VISIT)
@@ -13,5 +14,23 @@ class VerifyController < ApplicationController
 
   def retry
     flash.now[:error] = I18n.t('idv.errors.fail')
+  end
+
+  def activated
+    redirect_to verify_url unless active_profile?
+  end
+
+  def fail
+    redirect_to verify_url unless idv_attempter.exceeded?
+  end
+
+  private
+
+  def require_no_active_profile
+    redirect_to profile_url if active_profile?
+  end
+
+  def active_profile?
+    current_user.active_profile.present?
   end
 end
